b0d023b8213a2751c82cf24f56e1c2156c6e1918
Eva Ly Kok <evi.lyke@gmail.com>
2015-12-28 14:18:24 +0100

    Second Commit
    Began coding the CLI + HmacSHA[384|512]

--- TP_Crypto/src/interpret/Interpret.java
+++ TP_Crypto/src/interpret/Interpret.java
@@ -1,9 +1,18 @@
 package interpret;
 
-public class Interpret {
+import org.apache.commons.cli.Options;
 
-    public static void main(String[] args) {
-        
-    }
-    
+public class Interpret {
+	
+	Options options = new Options();
+	
+	/*
+	 * options : -sha1, -sha256, -sha384, -sha512, -md5, -pbesha1 -pbesha256, -pbesha384, -pbesha512, -pbemd5
+	 * arguments : path to file to code or verify + password (optional)
+	 */
+	
+	public Interpret(String[] args){
+		
+	}
+	
 }
--- TP_Crypto/src/mac/HmacSHA.java
+++ TP_Crypto/src/mac/HmacSHA.java
@@ -32,4 +32,25 @@ public class HmacSHA {
 		out.close();
 		in.close();
 	}
+	
+	public void HmacSHA512(File file, String password) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, IOException{
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+		SecretKeySpec ks = new SecretKeySpec(password.getBytes(), "HmacSHA512");
+		Mac mac = Mac.getInstance("HmacSHA512");
+		
+		mac.init(ks);
+		byte[] buffer = new byte[1024];
+		int l = 0;
+		
+		while((l = in.read()) != -1){
+			mac.update(buffer, 0, l);
+		}
+		
+		byte[] tag = mac.doFinal();
+		FileOutputStream out = new FileOutputStream(file.getName() + "_HmacSHA512Tag");
+		out.write(tag);
+		
+		out.close();
+		in.close();
+	}
 }
